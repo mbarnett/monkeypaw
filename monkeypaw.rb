ee3ba821f4b2961c22bff51014be7202f6772d8f
Matt Barnett <matt@sixtyodd.com>
2013-12-13 15:24:54 -0700

    this is probably more readable until I split out some templates

--- monkeypaw.rb
+++ monkeypaw.rb
@@ -2,11 +2,61 @@
 
 require 'thor'
 
-
 class MonkeyPaw < Thor
-    include Thor::Actions
+  include Thor::Actions
 
-no_commands {
+  desc "rack", "Creates a new Rack middleware project"
+  def rack(name)
+    empty_directory "rack-#{name}"
+
+    inside "rack-#{name}" do
+      empty_directory 'lib'
+
+      create_file '.gitignore' do
+        dot_gitignore
+      end
+
+      inside 'lib' do
+        create_file "rack-#{name}.rb" do
+          rack_toplevel(name)
+        end
+        empty_directory 'rack'
+        inside 'rack' do
+          empty_directory name
+          inside name do
+            create_file 'version.rb' do
+              rack_version_rb(name)
+            end
+          end
+          create_file "#{name}.rb" do
+            rack_main(name)
+          end
+        end
+      end
+
+      empty_directory 'spec'
+
+      create_file 'Rakefile' do
+        rakefile
+      end
+
+      create_file 'README.md'
+      create_file 'LICENSE'
+      create_file 'Gemfile' do
+        gemfile
+      end
+      create_file 'VERSION' do
+        version
+      end
+      create_file "rack-#{name}.gemspec" do
+        gemspec(name)
+      end
+
+      run 'git init'
+    end
+  end
+
+  no_commands {
 def rack_toplevel(name)
 <<-RACK_TOPLEVEL
 require 'rack/#{name}'
@@ -83,7 +133,7 @@ Gem::Specification.new do |s|
   s.version = File.read('VERSION')
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
-  s.authors = ["John Doe"]
+  s.authors = ["Fakey McFakerson"]
   s.date = Date.today.to_s
   s.description = %q{A rack middleware for <insert description here>}
   s.summary = %q{A rack middleware for <insert summary>}
@@ -130,56 +180,6 @@ end
 
 }
 
-    desc "rack", "Creates a new Rack middleware project"
-    def rack(name)
-      empty_directory "rack-#{name}"
-
-      inside "rack-#{name}" do
-        empty_directory 'lib'
-
-        create_file '.gitignore' do
-          dot_gitignore
-        end
-
-        inside 'lib' do
-          create_file "rack-#{name}.rb" do
-            rack_toplevel(name)
-          end
-          empty_directory 'rack'
-          inside 'rack' do
-            empty_directory name
-            inside name do
-              create_file 'version.rb' do
-                rack_version_rb(name)
-              end
-            end
-            create_file "#{name}.rb" do
-              rack_main(name)
-            end
-          end
-        end
-
-        empty_directory 'spec'
-
-        create_file 'Rakefile' do
-          rakefile
-        end
-
-        create_file 'README.md'
-        create_file 'LICENSE'
-        create_file 'Gemfile' do
-          gemfile
-        end
-        create_file 'VERSION' do
-          version
-        end
-        create_file "rack-#{name}.gemspec" do
-          gemspec(name)
-        end
-
-        run 'git init'
-      end
-    end
 end
 
 
